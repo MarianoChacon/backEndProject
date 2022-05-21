74012c4e23dde8fbf02c9f6ca1b0595cb3be4ac7
MarianoChacon <marianofchacon@gmail.com>
2022-05-21 15:17:33 -0300

    hasta CRUD de posteos funcionando

--- blogVino/appPosteos/urls.py
+++ blogVino/appPosteos/urls.py
@@ -4,7 +4,12 @@ from appPosteos.views import *
 
 urlpatterns = [
 
-    path('', PosteoList.as_view(), name='List'),
+    path('posteo/list/', PosteoList.as_view(), name='list'),
+    path('posteo/<pk>', PosteoDetalle.as_view(), name='posteo_detalle'),
+    path('posteo/nuevo/', PosteoCrear.as_view(), name='posteoCrear'),
+    path('posteo/editar/<pk>', PosteoEditar.as_view(), name='posteoEditar'),
+    path('posteo/eliminat/<pk>', PosteoEliminar.as_view(), name='posteoEliminar'),
+    
     
 
 ]
--- blogVino/appPosteos/views.py
+++ blogVino/appPosteos/views.py
@@ -2,12 +2,43 @@ from msilib.schema import ListView
 from django.shortcuts import render
 from django.views.generic import ListView
 from appPosteos.models import *
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+
+
 # Create your views here.
 
 
+def prueba(request):
+    return render(request, 'AppPosteos/prueba.html')
+
+#------------------CREATE--------------
+
+class PosteoCrear(CreateView):
+    model = Posteo
+    success_url = reverse_lazy('list')
+    fields=['titulo', 'subtitulo', 'cuerpo','autor','resumen','imagen']
 
 #------------------READ--------------
 
 class PosteoList(ListView):
-    Model = Posteo
-    template_name = 'appPosteos/posteos.html'
+    model = Posteo
+    template_name = 'appPosteos/posteo_list.html'
+
+class PosteoDetalle(DetailView):
+    model = Posteo
+    template_name = 'appPosteos/posteo_detalle.html'
+
+#-----------------UPDATE---------------------
+
+class PosteoEditar(UpdateView):
+    model = Posteo
+    success_url = reverse_lazy('list')
+    fields=['titulo', 'subtitulo', 'cuerpo','autor','resumen','imagen']
+
+#-----------------DELETE---------------------
+
+class PosteoEliminar(DeleteView):
+    model = Posteo
+    success_url = reverse_lazy('list')
